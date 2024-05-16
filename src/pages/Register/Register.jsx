94aacfbb6d0f1e57dd735373f64ece567cbb4bd7
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-16 19:55:51 +0300

    Styled Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,14 @@
 import Form from 'components/Form/Form';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BackLink, RegisterContainer } from './Register.styled';
 
 const Register = () => {
   return (
-    <div>
-      <button>Co back</button>
+    <RegisterContainer>
+      <BackLink to="/">Go back</BackLink>
       <Form />
-    </div>
+    </RegisterContainer>
   );
 };
 
--- src/pages/Register/Register.styled.js
+++ src/pages/Register/Register.styled.js
@@ -1,1 +1,17 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+export const RegisterContainer = styled.div`
+  padding: 20px;
+`;
+
+export const BackLink = styled(Link)`
+  color: #3c5bb0;
+  text-decoration: none;
+  margin-bottom: 20px;
+  display: block;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
